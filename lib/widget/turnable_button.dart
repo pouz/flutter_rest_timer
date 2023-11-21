46f355ab683a6e0488c2b371bbf1c6ee4f1d516b
Pouz <whitewyrm84@gmail.com>
2023-11-22 01:14:02 +0900

    dont use

--- lib/widget/turnable_button.dart
+++ lib/widget/turnable_button.dart
@@ -8,30 +8,23 @@ class TurnableButton extends StatelessWidget {
     this.disableBackgroundColor = Colors.grey,
     this.enable = true,
     this.padding = const EdgeInsets.all(8),
-    this.child,
+    required this.child,
   });
 
   final void Function()? onPressed;
   final ButtonStyle? style;
   final Color? disableBackgroundColor;
   final bool enable;
-  final Widget? child;
+  final Widget child;
   final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: padding!,
-      child: ElevatedButton(
+      child: IconButton(
         onPressed: onPressed,
-        style: enable
-            ? style
-            : ElevatedButton.styleFrom().copyWith(
-                backgroundColor:
-                    MaterialStateProperty.all(disableBackgroundColor),
-                elevation: MaterialStateProperty.all(0),
-              ),
-        child: child,
+        icon: child,
       ),
     );
   }
